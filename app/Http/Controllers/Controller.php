3071d300ad96cefe0651bf63a8af2c2e08bd68e7
Alex P <alexperis95@gmail.com>
2025-01-23 10:29:15 +0100

    feat: :sparkles: Test CRUD methods and methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Log;
+
 abstract class Controller
 {
-    //
+    public function LogError($message, $code){
+        Log::error($message);
+        return response()->json(['error' => 'Ha ocurrido un error'], $code);
+    }
 }
