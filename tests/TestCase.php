4875b4041531dfb10a6b5eca91560e60794fad7c
Alex P <alexperis95@gmail.com>
2025-01-27 11:01:17 +0100

    User CRUD Test and TestCase refactor

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,35 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication, RefreshDatabase;
+
+    protected $admin;
+    protected $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Artisan::call('migrate:fresh');  
+        $this->initialize_tests();
+    }
+
+    public function initialize_tests(): void
+    {
+        Role::firstOrCreate(['name' => 'admin']);
+        Role::firstOrCreate(['name' => 'user']);
+
+        $this->admin = User::factory()->create();
+        $this->admin->assignRole('admin');
+
+        $this->user = User::factory()->create();
+        $this->user->assignRole('user');
+    }
 }
