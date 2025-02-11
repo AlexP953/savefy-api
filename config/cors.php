d1f03d1ed335afd6c9a8a7348df9b6106dbc0faf
Alex P <alexperis95@gmail.com>
2025-02-11 11:28:27 +0100

    update cors

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'], // Rutas donde se aplicará CORS
-    'allowed_methods' => ['*'], // Permitir todos los métodos (GET, POST, etc.)
-    'allowed_origins' => ['*'], // Puedes poner '*' o los dominios permitidos
+    'paths' => ['api/*', 'sanctum/csrf-cookie'], 
+    'allowed_methods' => ['*'], 
+    'allowed_origins' => ['*'], 
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'], // Permitir todos los headers
+    'allowed_headers' => ['*'], 
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true, // Cambiar a `true` si usas autenticación con cookies
+    'supports_credentials' => true, 
 ];
