f027c5b801f9954f0ca9f03949ee0822e71a4fb9
Alex P <alexperis95@gmail.com>
2025-02-18 11:41:59 +0100

    fix: :bug: fix create user

--- routes/users.php
+++ routes/users.php
@@ -5,13 +5,13 @@ use App\Http\Controllers\UserController;
 
 Route::middleware('auth:api')->get('/user', [UserController::class, 'show']);
 
+Route::post('/users/create-user', [UserController::class, 'store']); // SIN auth:api
+
 Route::middleware('auth:api')->prefix('users')->group(function () {
 
   Route::middleware('auth:api')->get('/', [UserController::class, 'index']);
   Route::middleware('auth:api')->get('/id/{id}', [UserController::class, 'getUserById']);
   Route::middleware('auth:api')->get('/email', [UserController::class, 'getUserByEmail']);
-
-  Route::middleware('auth:api')->post('/create-user', [UserController::class, 'store']);
 
   Route::middleware('auth:api')->patch('/update', [UserController::class, 'update']);
 
